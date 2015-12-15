71fb3c734cd983593ec6af76f3af99b86e093dfe
Cameron <Cameron@GHOST.SMALLWIFI.local>
2015-12-15 21:19:34 +0000

    Split live and vod player modules into their own files Removed useless schedule plugin

--- source/admin/plugins/video_live.php
+++ source/admin/plugins/video_live.php
@@ -6,3 +6,276 @@
  * and open the template in the editor.
  */
 
+class live extends optionsPage{
+    
+    public $name = "plugin_live";
+    public $title = "Live Streaming";
+    
+    public function configPage(){
+        global $connection;
+        ce::begin("ce-medium;");
+        if($stream = filter_input(INPUT_GET,"edit")){
+            if($stmt = $connection->prepare("SELECT title,type,mobile,url,source,description,cover FROM plugin_live WHERE id=?")){
+                $stmt->bind_param('i',$stream);
+                $stmt->execute();
+                $stmt->store_result();
+                $stmt->bind_result($title,$type,$mobile,$url,$source,$description,$cover);
+                $stmt->fetch();
+                
+                backButton($this->name);
+                $editForm = new ajaxForm("editVideoForm", $this->name . "&edit=" . $stream, "POST");
+                $editForm->formTitle("Edit stream");
+                $editForm->labeledInput("title", "text", $title, "Title");
+                $editForm->kpSelector("type", self::kpStreamTypes(), $type, "Stream type");
+                $editForm->kpSelector("mobile", self::kpOtherStreams($stream), $mobile, "Mobile stream");
+                $editForm->labeledInput("url", "text", $url, "Stream URL");
+                $editForm->kpSelector("cover", cover::kpCovers(), $cover, "Loading screen");
+                 ajaxForm::startOptionalSection("sourceSection", "Custom code");
+                $editForm->plainText("source", $source, "Custom embed code");
+                ajaxForm::endOptionalSection();
+                $editForm->largeText("description", $description, "Description");
+                $editForm->lockedInput(actualLink() . "/public.php?action=$this->name&id=$stream", "External embed URL");
+                $editForm->otherActionButton("deleteVideo", "Delete stream", "&delete=$stream");
+                $editForm->submit("Save changes");
+            }else{
+                echo "Error accessing database";
+            }
+        }else{
+        
+            $form = new ajaxForm("newItemForm", $this->name, "POST");
+            $form->formTitle("New stream");
+            $form->labeledInput("title", "text", "", "Title");
+            $form->kpSelector("type", self::kpStreamTypes(), "custom", "Stream type");
+            $form->kpSelector("mobile", self::kpStreams(), "", "Mobile stream");
+            $form->labeledInput("url", "text", "", "Stream URL");
+            $form->kpSelector("cover", cover::kpCovers(), "", "Loading screen");
+            ajaxForm::startOptionalSection("sourceSection", "Custom code");
+            $form->plainText("source", "", "Custom embed code");
+            ajaxForm::endOptionalSection();
+            $form->largeText("description", $details['description'], "Description");
+            $form->submit("Add new item");
+            
+            $videos = self::allStreams();
+            $list = new multiPageList($videos,"videoList");
+            $list->title("All streams");
+            
+            $list->display($this->name);
+        }
+        ce::end();
+    }
+    
+    public function updatePage(){
+        global $connection;
+        
+        $title = filter_input(INPUT_POST,"title");
+        $type = filter_input(INPUT_POST,"type");
+        $mobile = filter_input(INPUT_POST,"mobile");
+        $url = filter_input(INPUT_POST,"url");
+        $source = content("source");
+        $description = content("description");
+        $cover = filter_input(INPUT_POST,"cover");
+
+        if(isset($_GET['delete'])){
+            
+            $delete = filter_input(INPUT_GET,"delete");
+            block(2);
+            
+            $bdstmt = $connection->prepare("DELETE FROM $this->name WHERE id=?");
+            $bdstmt->bind_param("i",$delete);
+            if($bdstmt->execute()){
+                echo "reload";
+            }else{
+                echo "Unable to delete stream: $bdstmt->error";
+            }
+        }
+        else if(isset($_GET['edit'])){
+            block(2);
+            
+            $edit = filter_input(INPUT_GET,"edit");
+            //Editing existing post
+            $equery = "UPDATE $this->name SET title=?, type=?, mobile=?, url=?, source=?, description=?, cover=?  WHERE id=?";
+            if($estmt = $connection->prepare($equery)){
+                $estmt->bind_param("ssisssii",$title,$type,$mobile,$url,$source,$description,$cover,$edit);
+                if($estmt->execute()){
+                    echo "Saved changes";
+                    $estmt->close();
+                    return;
+                }
+            }
+            echo "Error saving changes: $estmt->error";
+            return;
+            
+        }
+        else{
+            block(2);
+            //Creating new stream
+            if(!$title){
+                echo "Please enter a title";
+                return;
+            }else{
+                $nbstmt = $connection->prepare("INSERT INTO $this->name (title,type,mobile,url,source,description,cover) VALUES (?,?,?,?,?,?,?);");
+                $nbstmt->bind_param("ssisssi",$title,$type,$mobile,$url,$source,$description,$cover);
+                if($nbstmt->execute()){
+                    echo "reload";
+                    return;
+                }
+                echo "Error adding post: $nbstmt->error";
+            }
+        }
+    }
+    
+    public function displayPage(){
+        if(isset($_GET['list'])){
+            echo json_encode(self::listStreams());
+        }else if(isset($_GET['id'])){
+            $streamid = filter_input(INPUT_GET,"id");
+            $stream = self::getStream($streamid);
+            iframeOutput($stream['title'], $stream['source']);
+        }else{
+            echo "Live stream iFrame generator<br />", PHP_EOL;
+            echo "Valid requests:<br />";
+            echo "&list : Returns list of streams in JSON format<br />";
+            echo "&id=id : Returns a particular stream's source code";
+        }
+    }
+    
+    public static function kpStreamTypes(){
+        return array("hds" => "HDS", "hls" => "HLS", "rtmp" => "RTMP","icecast" => "Radio", "custom" => "Custom embed code" );
+    }
+    public static function getStream($id){
+        global $connection;
+        if($stmt = $connection->prepare("SELECT title,type,url,mobile,source,description,cover FROM plugin_live WHERE id=?")){
+            $stmt->bind_param("i",$id);
+            $stmt->execute();
+            $stmt->store_result();
+            $stmt->bind_result($title, $type, $url, $mobile, $code, $description,$cover);
+            $stmt->fetch();
+            
+            
+            $cover = cover::getImage($cover);
+            $cover_url = $cover->poster;
+            if($mobile && onMobile()){
+              $mobStream = self::getStream($mobile);
+              $source = $mobStream['source'];
+              $description = $mobStream['description'];
+            }else{
+                
+                
+                
+                switch($type){
+                    case "hds":
+                        $source = self::hdsStream($url,$cover_url);
+                        break;
+                    case "hls":
+                        $source = self::hlsStream($url,$cover_url);
+                        break;
+                    case "rtmp":
+                        $source = self::rtmpStream($url,$cover_url);
+                        break;
+                    case "icecast":
+                        require_once('plugins/radio/player.php');
+                        $source = self::icecastStream($url, $cover_url, $code, $title);
+                        $title = radioPlayer::getNowPlaying($code, $title);
+                        break;
+                    default:
+                        $source = $code;
+                        break;
+                }
+            }
+            
+            $video = new video($id, $type, null, $source, null, $title, $description, "", "");
+            
+            return $video;
+        }else{
+            return false;
+        }
+    }
+    public static function kpStreams(){
+        global $connection;
+        if($stmt=$connection->prepare("SELECT id,title FROM plugin_live")){
+            $stmt->execute();
+            $stmt->bind_result($id,$title);
+            $streams = array();
+            $streams['null'] = "--";
+            while($stmt->fetch()){
+                $streams[$id] = $title;
+            }
+            return $streams;
+            
+        }else{
+            return false;
+        }
+    }
+    public static function kpOtherStreams($exclude){
+        $array = self::kpStreams();
+        unset($array[$exclude]);
+        return $array;
+    }
+    public static function allStreams(){
+        /*
+         * Returns list of streams for use in stream editor
+         */
+        global $connection;
+        if($stmt=$connection->prepare("SELECT id,title,type,mobile,url,source,description,cover FROM plugin_live")){
+            $stmt->execute();
+            $stmt->bind_result($id,$title,$type,$mobile,$url,$source,$description,$cover);
+            $streams = array();
+            while($stmt->fetch()){
+                $onclick = "cm_loadPage('plugin_live&edit=$id');";
+                $streams[] = array("Title" => $title, "Type" => self::kpStreamTypes()[$type], "onclick" => $onclick);
+            }
+            return $streams;
+            
+        }else{
+            return false;
+        }
+    }
+    /**
+     * 
+     * @global type $connection
+     * @return boolean
+     * 
+     * Returns list of streams for JSON reply
+     */
+     public static function listStreams(){
+        global $connection;
+        if($stmt=$connection->prepare("SELECT id,title,type,mobile,url,source,description,cover FROM plugin_live")){
+            $stmt->execute();
+            $stmt->bind_result($id,$title,$type,$mobile,$url,$source,$description,$cover);
+            $cover_url = cover::getImage($cover);
+            $streams = array();
+            while($stmt->fetch()){
+                $onclick = "cm_loadPage('plugin_live&edit=$id');";
+                $streams[] = array("id" => $id, "title" => $title, "type" => $type, "url"  => $url, "source" => $source, "description" => $description, "cover" => $cover_url);
+            }
+            return $streams;
+            
+        }else{
+            return false;
+        }
+    }
+    
+    /*Individual stream code generators
+     * hdsStream - HDS, requires OSMF
+     */
+    public static function hdsStream($url){
+        return "<video class=\"vidplayer video-js vjs-default-skin html5vid\" width=\"100%\" height=\"100%\" poster=\"$cover\" controls autoplay data-setup='{\"techOrder\": [\"flash\",\"html5\"]}'>" . "<source src=\"$url\" type=\"application/adobe-f4m\">" . "Your browser does not support the video tag" . "</video>";
+    }
+    public static function hlsStream($url,$cover){
+        //Designed for videoJS with FlasHLS
+        return "<video class=\"vidplayer video-js vjs-default-skin html5vid\" width=\"100%\" height=\"100%\" poster=\"$cover\" controls autoplay data-setup='{\"techOrder\": [\"flash\",\"html5\"]}'>" . "<source src=\"$url\" type=\"application/x-mpegURL\">" . "Your browser does not support the video tag" . "</video>";
+    }
+    public static function rtmpStream($url,$cover){
+        return "<video class=\"vidplayer video-js vjs-default-skin html5vid\" width=\"100%\" height=\"100%\" poster=\"$cover\" controls autoplay>" . "<source src=\"$url\" type=\"rtmp/mp4\">" . "Your browser does not support the video tag" . "</video>";        
+    }
+    public static function shoutStream($url,$cover){
+        //Deprecated
+        return "<video class=\"vidplayer radplayer video-js vjs-default-skin html5vid\" width=\"100%\" height=\"100%\" data-setup='{ \"inactivityTimeout\": 0, \"allowfullscreen\":\"false\" }' poster=\"$cover\" controls autoplay>" . "<source src=\"$url\" type=\"audio/mp3\">" . "</video>";
+    }
+    public static function icecastStream($url,$cover,$nowplaying, $title){
+        //Uses external plugin - do not try to run if radio folder is not installed!
+        require_once('plugins/radio/player.php');
+        return radioPlayer::build($url,$cover,$nowplaying, $title);
+    }
+}
+$pluginPages[] = new live();
--- source/admin/plugins/video_vod.php
+++ source/admin/plugins/video_vod.php
@@ -6,3 +6,256 @@
  * and open the template in the editor.
  */
 
+class videos extends optionsPage{
+    
+    public $name = "plugin_vod";
+    public $title = "On Demand Videos";
+    
+    public function configPage(){
+        global $connection;
+        ce::begin("ce-medium;");
+        if($video = filter_input(INPUT_GET,"edit")){
+            
+            backButton($this->name);
+            $details = self::getVideo($video);
+            $editForm = new ajaxForm("editVideoForm", $this->name . "&edit=" . $video, "POST");
+            $editForm->formTitle("Edit video");
+            $editForm->labeledInput("title", "text", $details['title'], "Title");
+            $editForm->kpSelector("type", mediaPlayer::kpTypes(), $details['type'], "Video type");
+            $editForm->labeledInput("url", "text", $details['url'], "Video URL/ID");
+            ajaxForm::startOptionalSection("embed","Embed code (Custom code)");
+            $editForm->plainText("code", $details['code'], "Custom embed code");
+            ajaxForm::endOptionalSection();
+            $editForm->labeledInput("length", "text", $details['length'], "Video length");
+            $editForm->labeledInput("date", "date", $details['date'], "Date posted");
+            $editForm->labeledInput("poster", "text", $details['poster'], "Poster URL");
+            $editForm->largeText("description", $details['description'], "Description");
+            $editForm->lockedInput(actualLink() . "/public.php?action=$this->name&id=$video", "External embed URL");
+            $editForm->otherActionButton("deleteVideo", "Delete video", "&delete=$video");
+            $editForm->submit("Save changes");
+        }else{
+        
+            $form = new ajaxForm("newItemForm", $this->name, "POST");
+            $form->formTitle("New video");
+            $form->labeledInput("title", "text", "", "Title");
+            $form->kpSelector("type", mediaPlayer::kpTypes(), $current, "Video type");
+            $form->labeledInput("url", "text", "", "Video URL/ID");
+            ajaxForm::startOptionalSection("embed","Embed code (Custom code)");
+            $form->plainText("code", "", "Custom embed code");
+            ajaxForm::endOptionalSection();
+            $form->labeledInput("length", "text", "", "Video length");
+            $nowdate = date("Y-m-d");
+            $form->labeledInput("date", "date", $nowdate, "Date posted");
+            $form->labeledInput("poster", "text", "", "Poster URL");
+            $form->largeText("description", "", "Description");
+            $form->submit("Add new item");
+            
+            $videos = self::allVideos();
+            $list = new multiPageList($videos,"videoList");
+            $list->title("All videos");
+            
+            $list->display($this->name);
+        }
+        ce::end();
+    }
+    
+    public function updatePage(){
+        global $connection;
+        
+        $title = filter_input(INPUT_POST,"title");
+        $length = filter_input(INPUT_POST,"length");
+        $date = filter_input(INPUT_POST,"date");
+        $description = content("description");
+        $type = filter_input(INPUT_POST,"type");
+        $url = filter_input(INPUT_POST,"url");
+        $code = content("code");
+        $poster = filter_input(INPUT_POST, "poster");
+       
+        
+        if(isset($_GET['delete'])){
+            
+            $delete = filter_input(INPUT_GET,"delete");
+            block(2);
+            
+            $bdstmt = $connection->prepare("DELETE FROM $this->name WHERE id=?");
+            $bdstmt->bind_param("i",$delete);
+            if($bdstmt->execute()){
+                echo "reload";
+            }else{
+                echo "Unable to delete video: $bdstmt->error";
+            }
+        }
+        else if(isset($_GET['edit'])){
+            
+            block(2);
+            $edit = filter_input(INPUT_GET,"edit");
+            //Editing existing post
+            if($bstmt = $connection->prepare("UPDATE $this->name SET title=?, length=?, date=?, description=?, type=? ,url=?, source=?, poster=?  WHERE id=?")){
+                $bstmt->bind_param("ssssssssi",$title,$length,$date,$description,$type,$url,$code,$poster,$edit);
+                if($bstmt->execute()){
+                    echo "Saved changes";
+                    return;
+                }
+            }
+            echo "Error saving changes: $bstmt->error";
+            return;
+            
+        }
+        else{
+            block(2);
+            //Creating new video
+            if(!$title){
+                echo "Please enter a title";
+                return;
+            }else{
+                $nbstmt = $connection->prepare("INSERT INTO $this->name (title,length,date,description,type,url,source,poster) VALUES (?,?,?,?,?,?,?,?);");
+                $nbstmt->bind_param("ssssssss",$title,$length,$date,$description,$type,$url,$source,$poster);
+                if($nbstmt->execute()){
+                    echo "reload";
+                    return;
+                }
+                echo "Error adding post: $nbstmt->error";
+            }
+        }
+    }
+    
+    public function displayPage(){
+        if(isset($_GET['list'])){
+            echo json_encode(self::listVideos());
+        }else if(isset($_GET['id'])){
+            //html::div("player_container","player1");
+            $videoid = filter_input(INPUT_GET,"id");
+            $video = self::getVideo($videoid);
+            iframeOutput($video->title, $video->source);
+        }
+    }
+
+    public static function kpVideos(){
+        global $connection;
+        if($stmt = $connection->prepare("SELECT id,title FROM plugin_vod ORDER BY id DESC")){
+            $stmt->execute();
+            $videos = array();
+            $stmt->bind_result($id,$vtitle);
+            $videos['null'] = "--";
+            while($stmt->fetch()){
+                $videos[$id] = $vtitle;
+            }
+            return $videos;
+        }else{
+            return false;
+        }
+    }
+    
+    public static function allVideos(){
+        /* Returns all video information in a multi-dimensional array */
+        global $connection;
+        if($stmt = $connection->prepare("SELECT id,title,type,length,date FROM plugin_vod ORDER BY id DESC")){
+            $stmt->execute();
+            $videos = array();
+            $stmt->bind_result($id,$vtitle,$type,$length,$date);
+            while($stmt->fetch()){
+                 $onclick = "cm_loadPage('plugin_vod&edit=$id');";
+                $videos[] = array("Title" => $vtitle, "Type" => mediaPlayer::kpTypes()[$type], "Length" => $length, "Date posted" => $date, "onclick" => $onclick);
+            }
+            return $videos;
+        }else{
+            return false;
+        }
+    }
+    /**
+     * 
+     * @global type $connection
+     * @return array
+     * 
+     * Returns all video information for a JSON array
+     */
+    public static function listVideos(){
+        /* Returns all video information in a multi-dimensional array ordered by date */
+        global $connection;
+        if($stmt = $connection->prepare("SELECT id,title,url,type,length,date FROM plugin_vod ORDER BY date desc")){
+            $stmt->execute();
+            $ids = array();
+            $videos = array();
+            $stmt->bind_result($id,$vtitle,$url,$type,$length,$date);
+            while($stmt->fetch()){
+                $ids[] = $id;
+            }
+            foreach($ids as $id){
+                //Writes other videos to array
+                $video = self::getVideo($id);
+                $video->video_id = $id;
+                $videos[] = $video;
+            }
+            return $videos;
+        }else{
+            return false;
+        }
+    }
+
+    /*
+    public static function kpTypes(){
+        return array("html5" => "HTML 5", "iframe" => "IFrame Embed", "custom" => "Custom embed code", "youtube" => "YouTube ID");
+    } */
+    
+    
+    
+   
+    
+    public static function getVideo($id){
+        global $connection;
+        
+        $video = array();
+        $video['id'] = "vod_" . $id;
+        $video['video_id'] = $id;
+        
+        
+        if($vstmt = $connection->prepare("SELECT title,length,date,description,type,url,source,poster FROM plugin_vod WHERE id=?")){
+            $vstmt->bind_param("i",$id);
+            $vstmt->execute();
+            $vstmt->bind_result($title,$length,$date,$desc,$type,$url,$source,$poster);
+            $vstmt->store_result();
+            $vstmt->fetch();
+            
+            $sources = array();
+            $source1 = new source($url,'video/mp4','720');
+            $sources[] = $source1;
+            $tags = "";
+            
+            $video = new video($id, $type, $sources, $source, $poster, $title, $desc, $date, $tags);
+            
+            $players = mediaPlayer::getPlayerTypes();
+            foreach($players as $player){
+                if($player->name == $type){
+                    //TODO - add setup 
+                    $video = $player->build($video,"");
+                }
+            }
+            return $video;
+            /*
+            switch($type){
+                case "iframe":
+                    $video['source'] = self::iframeCode($url);
+                    break;
+                case "html5":
+                    $video['source'] = self::htmlCode($url,$poster);
+                    break;
+                case "youtube":
+                    $video['source'] = self::ytCode($url);
+                    if(!$poster){ 
+                        $video['poster'] = "http://img.youtube.com/vi/$url/maxresdefault.jpg";
+                    }
+                    break;
+                default:
+                    $video['source'] = $source;
+                    break;
+            }
+            */
+            //return $video;
+        }else{
+            echo "Error - VOD database request failed!";
+        }
+    }
+    
+}
+
+$pluginPages[] = new videos();
