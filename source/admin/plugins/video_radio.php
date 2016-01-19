2e456e74cc6cd3b9f49169cd46a7be637cfe0a12
Cameron <Cameron@Spectre.SMALLWIFI.local>
2016-01-19 22:24:46 +0000

    Added new player for visualised radio with support for nowplaying info fetching

--- source/admin/plugins/video_radio.php
+++ source/admin/plugins/video_radio.php
@@ -37,3 +37,68 @@ class radio extends mediaPlayer{
     }
     
 }
+
+class visual_radio extends mediaPlayer{
+    
+    public $name = 'radio_visual';
+    
+    public $title = 'LCR Video Player';
+    public $live = true;
+    public $vod = true;
+    
+    static function build($content,$setup){
+        $primarySource = $content->sources[0];
+        $url= $primarySource->src;
+
+        $poster = $content->poster;
+        $nowplaying_url = $content->source;
+        
+        require_once('plugins/radio/player.php');
+
+        //$title = radioPlayer::getNowPlaying($nowplaying, $content->title);
+
+        $info = radioPlayer::getNowPlaying($nowplaying_url);
+        if(strlen($info['title']) > 0){
+            $content->server_info = $info['raw'];
+            $content->nowplaying = $info['title'];
+            $content->plaintitle = $content->title;
+            $content->title = $content->title . ': ' . $info['title'];
+        }
+        //$content->poster = null;
+        
+        ob_start();
+        
+        html::css("plugins/radio/video-js-custom.css");
+        //html::js("plugins/video/videojs/core/video.min.js");
+        
+        html::js("//vjs.zencdn.net/5.3.0/video.js");	//CDN version
+       
+        
+        html::css('plugins/video/videojs/resolution-switcher/videojs-resolution-switcher.css');
+        html::js('plugins/video/videojs/resolution-switcher/videojs-resolution-switcher.js');
+       
+        
+        echo "<video id=\"video\" class=\"vidplayer video-js vjs-default-skin html5vid\" width=\"100%\" height=\"100%\" poster=\"$poster\" controls autoplay data-setup='{\"techOrder\": [\"html5\",\"flash\"] , \"plugins\": { \"videoJsResolutionSwitcher\" : { \"default\" : \"720\" } }}' $video->code>", PHP_EOL;
+        foreach($content->sources as $source){
+            $src = $source->src;
+            $type = $source->type;
+            $res = $source->res;
+            $label = $res . 'p';
+            echo "<source label=\"$label\" res=\"$res\" src=\"$src\" type=\"$type\" >", PHP_EOL;
+        }
+        echo "Your browser does not support the video tag";
+                
+        echo "</video>";
+        
+        //echo '<script>videojs(\'#video\').videoJsResolutionSwitcher</script>';
+        
+        $content->source =  ob_get_contents();
+        ob_end_clean();
+
+        $json = actualLink() . "/public.php?action=plugin_vod&id=$content->id";
+
+        
+
+        return $content;
+    }
+}
