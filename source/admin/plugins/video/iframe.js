6e216791dfe8349801e3220c62d8ce50f92e293f
Cameron <Cameron@Spectre.SMALLWIFI.local>
2016-01-21 01:19:14 +0000

    Updated iframe.js to catch exception when channelURL isn't defined. Notice will be output in console

--- source/admin/plugins/video/iframe.js
+++ source/admin/plugins/video/iframe.js
@@ -3,28 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-if(channelURL){
-    function checkContent(){
-        var request = new XMLHttpRequest();
-	request.onreadystatechange = function(){
-		if(request.readyState == 4 && request.status == 200){
-                    var string = request.responseText;
-                    var array = JSON.parse(string);
-                    //console.log('Old:' + nowPlayingId + ' New:' + array['id']);
-                    //update page
-                    if(nowPlayingId != array['id']){
-                        //console.log('Content has changed, reloading frame');
-                        document.location.reload(true);   
+try{  
+    if(channelURL){
+        function checkContent(){
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function(){
+                    if(request.readyState == 4 && request.status == 200){
+                        var string = request.responseText;
+                        var array = JSON.parse(string);
+                        //console.log('Old:' + nowPlayingId + ' New:' + array['id']);
+                        //update page
+                        if(nowPlayingId != array['id']){
+                            //console.log('Content has changed, reloading frame');
+                            document.location.reload(true);   
+                        }
+                        nowPlayingId = array['id'];
                     }
-                    nowPlayingId = array['id'];
-		}
-	}
-	request.open("GET",channelURL ,true);
-	request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	request.send();
-    }
-    
-    interval = window.setInterval(checkContent,10000);
-}
+            }
+            request.open("GET",channelURL ,true);
+            request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            request.send();
+        }
 
+        interval = window.setInterval(checkContent,10000);
+    }
+    }
+catch(err){
+    console.log('channelURL not defined');
+}
