def4f6e224a90b2a9ec651de023f4a99c131004b
Cameron <Cameron@131.231.224.115>
2016-02-26 23:11:15 +0000

    Added guide for twitter_keys installation

--- source/admin/plugins/twitter/twitterscreen.php
+++ source/admin/plugins/twitter/twitterscreen.php
@@ -6,70 +6,16 @@
  * and open the template in the editor.
  */
 
-class twitterscreen{
-
-    public static function build($feed,$image,$title){
-    
-        html::start();
-        
-        echo "<div id=\"background\"></div>";
-
-        html::css('plugins/twitter/twitterscreen.css');
-        html::endHead();
-        
-        echo "<iframe id=\"screen\" src=\"public.php?action=plugin_twitter&search=$feed\"></iframe>";
-
-        html::div('banner','banner');
-        echo "<span>$title</span>";
-        html::closeDiv();
-        
-
-        echo "<img class=\"biglogo\" src=\"$image\">";
-
-        html::end();
-    }
-
-}
-
-class twitterscroller{
-    public static function build($term){
-        html::start();
-
-        html::css('plugins/twitter/twitter.css');
-        html::js('plugins/twitter/twitter.js');
-        html::endHead();
-
-        html::div('wrapper','wrapper');
-            html::div('profilebar','profilebar');
-        
-
-                html::div('profilepic','profilepic');
-                html::closeDiv();
-                
-                html::div('namebox','namebox');
-                echo "<p id=\"name\"></p><p id=\"handle\"></p>";
-                html::closeDiv();
-                
-            html::closeDiv();
-            html::div('result','result');
-            echo "No tweets found yet!";
-            html::closeDiv();
-            
-        html::closeDiv();
-        
-        html::div('bgimage','bgimage');
-        echo "<img style=\"transform:translate(0,0);\" src=\"//falkegg.co.uk/images/bouleyblur.min.jpg\">";
-        html::closeDiv();
-          
-          echo "<script>", PHP_EOL;
-          echo "var term='$term';"
-                  . "loadAllTweets();"
-                  . "var timer1 = setInterval(loadNewTweet,10000);", PHP_EOL;
-          echo "</script>", PHP_EOL;
-          
-          html::end();
-    }
-}
+/*In order to use this plugin you must create a twitter_keys.php file containing
+ * your Twitter app keys
+ * It should contain an array of data formatted like below:
+ * $settings = array(
+    'oauth_access_token' => "oath_token",
+    'oauth_access_token_secret' => "oath_secret",
+    'consumer_key' => "consumer_key",
+    'consumer_secret' => "consumer_secret"
+);
+ */
 
 class twitterList{
     
