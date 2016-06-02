948eea84a0dab173ae59cec8c36c1094b5c31c12
Cameron <Cameron@131.231.224.115>
2016-06-02 13:52:12 +0100

    Add cache header to fix irritating MS Edge activity...

--- source/admin/public.php
+++ source/admin/public.php
@@ -21,6 +21,10 @@ require 'connect.php';
 require 'functions/data_wrangler.php';
 require 'functions/elements.php';
 
+//Prevent caching
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
 
 //Current internal pages
 
