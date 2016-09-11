d5cf3ea6c7870d8f78e0398b91114d8088025b42
Cameron Small <camerontsmall@googlemail.com>
2016-09-11 01:45:05 +0100

    Started work on new blog plugin - labeled old plugin as deprecated

--- source/admin/plugins/blog.php
+++ source/admin/plugins/blog.php
@@ -1,171 +1,59 @@
 <?php
 
-/*
+/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-/**
- * Description of blog
- * Simple blog plugin, can send posts with AJAX
- *
- * @author Cameron
- */
 class blog extends optionsPage{
-    public $name = "plugin_blog";
-    public $title = "Blog";
     
-    function setup(){
-        
+    public $name = 'blog';
+    public $title = 'Blog';
+   
+    public static function formArray(){
+        global $auth;
+        return [
+            'title' => ['type' => 'text', 'label' => 'Title', 'value' => ''],
+            'author' => ['type' => 'select', 'label' => 'Author', 'options' => self::select_users(), 'value' => $auth->server_get_profile()['username']],
+            'date' => ['type' => 'date', 'label' => 'Date', 'value' => Date('Y-m-d')],
+            'featured_image' => ['type' => 'file', 'label' => 'Thumbnail image', 'value' => ''],
+            'content' => ['type' => 'richtext', 'label' => 'Content', 'value' => ''],
+            
+        ];
     }
     
-    function displayPage(){
+    public static function select_users(){
+        $users = authenticator::server_get_users();
         
-        global $connection; //Import connection object
+        $clean = [];
         
-        if(isset($_GET['id'])){
-            $post = filter_input(INPUT_GET,'id');blic.
-            $pstmt = $connection->prepare("SELECT title, content, author, timestamp,tags FROM blog WHERE id=?");
-            $pstmt->bind_param('i',$post);
-            $pstmt->execute();
-            $pstmt->store_result();
-            $pstmt->bind_result($title,$content,$author,$timestamp,$tags);
-            $pstmt->fetch();
-            
-            $pArray = array("title" => $title, "author"=> $author, "tags" => $tags, "date" => $timestamp, "content" => $content);
-            $pJSON = json_encode($pArray);
-            echo $pJSON;
-            return true;
-            
-        }else{
-            echo "Pamphlet Blogs JSON Request<br />", PHP_EOL;
-            echo "&list: Returns list of posts<br />";
-            echo "&id=postid: Returns one post's data";
+        foreach($users as $user){
+            $clean[$user['username']] = $user['fullname'] . " ({$user['username']})";
         }
-        
-        
+        return $clean;
     }
     
-    function configPage(){
-        global $connection;
+    public function configPage() {
+        global $auth;
         ce::begin('');
         
-        if($post = filter_input(INPUT_GET,'post')){
-            /* Single post editor */
-            backButton($this->name);
-            $pstmt = $connection->prepare("SELECT title, content, author, timestamp,tags FROM blog WHERE id=?");
-            $pstmt->bind_param('i',$post);
-            $pstmt->execute();
-            $pstmt->store_result();
-            $pstmt->bind_result($title,$content,$author,$timestamp,$tags);
-            $pstmt->fetch();
-            $editBlog = new ajaxForm("editBlogForm", $this->name . "&edit=" . $post, "POST");
-            $editBlog->formTitle("Edit Blog Post");
-            $editBlog->labeledInput("title", "text", $title, "Title");
-            $editBlog->labeledInput("tags", "text", $tags, "Tags");
-            $editBlog->kpSelector("author", kpFullnames(), $author, "Author");
-            $editBlog->largeText("content", $content, "Content");
-            $editBlog->otherActionButton("deleteBlog", "Delete post", "&delete=$post");
-            $editBlog->submit("Update post");
-            $pstmt->close();
-            
-        }else{
+        $form = new customForm(self::formArray(), 'editForm', 'blog', 'POST', 'blog');
         
-            $newBlog = new ajaxForm("newBlogForm", $this->name, "POST");
-            $newBlog->formTitle("New blog post");
-            $newBlog->labeledInput("title", "text", "", "Title");
-            $newBlog->labeledInput("tags", "text", "", "Tags (space separated)");
-            $newBlog->kpSelector("author", kpFullnames(), get_username(), "Author");
-            $newBlog->largeText("content", "", "Content");
-            $newBlog->submit("Add post");
-            $newBlog->end();
-
-            $bstmt = $connection->prepare("SELECT b.id,b.title,b.author,b.timestamp,b.tags,u.fullname FROM blog b, users u WHERE b.author = u.username ORDER BY b.timestamp DESC");
-            $bstmt->execute();
-            $bstmt->bind_result($id,$title,$author,$postdate,$tags,$fullname);
-            $blogList = new ajaxList(null, "bloglist");
-            while($bstmt->fetch()){
-                $nicedate = date("jS F Y",strtotime($postdate));
-                $tagString = tagsToString($tags);
-                $post = array("Title" => $title, "Author" => $fullname, "Tags" => $tagString, "Posted" => $nicedate, "action" => "$this->name&post=$id");
-                $blogList->addObject($post);
-            }
-
-
-            $blogList->title("Current posts");
-            $blogList->display();
-            $bstmt->close();
-        }
+        $form->title = 'New blog post';
+        $form->build('Add post');
+        
+        
         ce::end();
-        
     }
-    function updatePage(){
-        global $connection;
+    
+    public function updatePage() {
         
-        $title = filter_input(INPUT_POST,"title");
-        $tags = filter_input(INPUT_POST,"tags");
-        $author = filter_input(INPUT_POST,"author");
-        $content = content("content");
-        
-        if(isset($_GET['delete'])){
-            
-            $delete = filter_input(INPUT_GET,"delete");
-            if(!blogAuthor($delete)){
-                block(2);
-            }
-            block(3);
-            
-            $bdstmt = $connection->prepare("DELETE FROM blog WHERE id=?");
-            $bdstmt->bind_param("i",$delete);
-            if($bdstmt->execute()){
-                echo "reload";
-            }else{
-                echo "Unable to delete post: $bdstmt->error";
-            }
-        }
-        else if(isset($_GET['edit'])){
-            if(!blogAuthor($delete)){
-                block(2);
-            }
-            block(3);
-            $edit = filter_input(INPUT_GET,"edit");
-            //Editing existing post
-            $bstmt = $connection->prepare("UPDATE blog SET title=?, author=?, tags=?, content=? WHERE id=?");
-            $bstmt->bind_param("ssssi",$title,$author,$tags,$content,$edit);
-            if($bstmt->execute()){
-                echo "Saved changes";
-                return;
-            }
-            echo "Error saving changes: $bstmt->error";
-            return;
-            
-        }
-        else{
-            block(3);
-            if(profile(get_username()['privilege']) < 3){
-                $author = get_username();
-            }
-            //Creating new post
-            if(!$title || !$author){
-                echo "Please enter a title";
-                return;
-            }else{
-                $nbstmt = $connection->prepare("INSERT INTO blog (title,content,author,tags) VALUES (?,?,?,?);");
-                $nbstmt->bind_param("ssss",$title,$content,$author,$tags);
-                if($nbstmt->execute()){
-                    echo "reload";
-                    return;
-                }
-                echo "Error adding post: $nbstmt->error";
-            }
-        }
-    }
-    function orphanage($olduser,$newuser){
+        $sql = customForm::insertSQL(self::formArray(), $_POST, 'blog');
+        echo $sql;
         
     }
     
 }
 
-$plugin_blog = new blog();
-$pluginPages[] = $plugin_blog;
+$pluginPages[] = new blog();
---
+++ source/admin/plugins/old_blog.php
@@ -0,0 +1,171 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of blog
+ * Simple blog plugin, can send posts with AJAX
+ *
+ * @author Cameron
+ */
+class old_blog extends optionsPage{
+    public $name = "plugin_blog";
+    public $title = "Blog (Deprecated)";
+    
+    function setup(){
+        
+    }
+    
+    function displayPage(){
+        
+        global $connection; //Import connection object
+        
+        if(isset($_GET['id'])){
+            $post = filter_input(INPUT_GET,'id');blic.
+            $pstmt = $connection->prepare("SELECT title, content, author, timestamp,tags FROM blog WHERE id=?");
+            $pstmt->bind_param('i',$post);
+            $pstmt->execute();
+            $pstmt->store_result();
+            $pstmt->bind_result($title,$content,$author,$timestamp,$tags);
+            $pstmt->fetch();
+            
+            $pArray = array("title" => $title, "author"=> $author, "tags" => $tags, "date" => $timestamp, "content" => $content);
+            $pJSON = json_encode($pArray);
+            echo $pJSON;
+            return true;
+            
+        }else{
+            echo "Pamphlet Blogs JSON Request<br />", PHP_EOL;
+            echo "&list: Returns list of posts<br />";
+            echo "&id=postid: Returns one post's data";
+        }
+        
+        
+    }
+    
+    function configPage(){
+        global $connection;
+        ce::begin('');
+        
+        if($post = filter_input(INPUT_GET,'post')){
+            /* Single post editor */
+            backButton($this->name);
+            $pstmt = $connection->prepare("SELECT title, content, author, timestamp,tags FROM blog WHERE id=?");
+            $pstmt->bind_param('i',$post);
+            $pstmt->execute();
+            $pstmt->store_result();
+            $pstmt->bind_result($title,$content,$author,$timestamp,$tags);
+            $pstmt->fetch();
+            $editBlog = new ajaxForm("editBlogForm", $this->name . "&edit=" . $post, "POST");
+            $editBlog->formTitle("Edit Blog Post");
+            $editBlog->labeledInput("title", "text", $title, "Title");
+            $editBlog->labeledInput("tags", "text", $tags, "Tags");
+            $editBlog->kpSelector("author", kpFullnames(), $author, "Author");
+            $editBlog->largeText("content", $content, "Content");
+            $editBlog->otherActionButton("deleteBlog", "Delete post", "&delete=$post");
+            $editBlog->submit("Update post");
+            $pstmt->close();
+            
+        }else{
+        
+            $newBlog = new ajaxForm("newBlogForm", $this->name, "POST");
+            $newBlog->formTitle("New blog post");
+            $newBlog->labeledInput("title", "text", "", "Title");
+            $newBlog->labeledInput("tags", "text", "", "Tags (space separated)");
+            $newBlog->kpSelector("author", kpFullnames(), get_username(), "Author");
+            $newBlog->largeText("content", "", "Content");
+            $newBlog->submit("Add post");
+            $newBlog->end();
+
+            $bstmt = $connection->prepare("SELECT b.id,b.title,b.author,b.timestamp,b.tags,u.fullname FROM blog b, users u WHERE b.author = u.username ORDER BY b.timestamp DESC");
+            $bstmt->execute();
+            $bstmt->bind_result($id,$title,$author,$postdate,$tags,$fullname);
+            $blogList = new ajaxList(null, "bloglist");
+            while($bstmt->fetch()){
+                $nicedate = date("jS F Y",strtotime($postdate));
+                $tagString = tagsToString($tags);
+                $post = array("Title" => $title, "Author" => $fullname, "Tags" => $tagString, "Posted" => $nicedate, "action" => "$this->name&post=$id");
+                $blogList->addObject($post);
+            }
+
+
+            $blogList->title("Current posts");
+            $blogList->display();
+            $bstmt->close();
+        }
+        ce::end();
+        
+    }
+    function updatePage(){
+        global $connection;
+        
+        $title = filter_input(INPUT_POST,"title");
+        $tags = filter_input(INPUT_POST,"tags");
+        $author = filter_input(INPUT_POST,"author");
+        $content = content("content");
+        
+        if(isset($_GET['delete'])){
+            
+            $delete = filter_input(INPUT_GET,"delete");
+            if(!blogAuthor($delete)){
+                block(2);
+            }
+            block(3);
+            
+            $bdstmt = $connection->prepare("DELETE FROM blog WHERE id=?");
+            $bdstmt->bind_param("i",$delete);
+            if($bdstmt->execute()){
+                echo "reload";
+            }else{
+                echo "Unable to delete post: $bdstmt->error";
+            }
+        }
+        else if(isset($_GET['edit'])){
+            if(!blogAuthor($delete)){
+                block(2);
+            }
+            block(3);
+            $edit = filter_input(INPUT_GET,"edit");
+            //Editing existing post
+            $bstmt = $connection->prepare("UPDATE blog SET title=?, author=?, tags=?, content=? WHERE id=?");
+            $bstmt->bind_param("ssssi",$title,$author,$tags,$content,$edit);
+            if($bstmt->execute()){
+                echo "Saved changes";
+                return;
+            }
+            echo "Error saving changes: $bstmt->error";
+            return;
+            
+        }
+        else{
+            block(3);
+            if(profile(get_username()['privilege']) < 3){
+                $author = get_username();
+            }
+            //Creating new post
+            if(!$title || !$author){
+                echo "Please enter a title";
+                return;
+            }else{
+                $nbstmt = $connection->prepare("INSERT INTO blog (title,content,author,tags) VALUES (?,?,?,?);");
+                $nbstmt->bind_param("ssss",$title,$content,$author,$tags);
+                if($nbstmt->execute()){
+                    echo "reload";
+                    return;
+                }
+                echo "Error adding post: $nbstmt->error";
+            }
+        }
+    }
+    function orphanage($olduser,$newuser){
+        
+    }
+    
+}
+
+$plugin_blog = new old_blog();
+$pluginPages[] = $plugin_blog;
